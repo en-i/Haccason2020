b824c78a900f89b404a1b01cd103af86845bc1e6
en-i <templebond@icloud.com>
2020-08-15 17:18:18 +0900

    first

---
+++ Library/Haccason/Assets/Script/Rotate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    //画像のX値
+    public int x = 0;
+
+    //画像のY値
+    public int y = 0;
+
+    //画像のZ値
+    public int z = 360;
+
+    public static bool decide;
+
+    Tweener tweener;
+
+    //回転させる速度
+    public int rotateSecond = 8;
+    // Start is called before the first frame update
+    void Start () {
+        decide = false;
+        StartCoroutine ("rotate");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(decide){
+            tweener.Kill();
+        }
+
+    }
+
+    IEnumerator rotate () {
+        //無限ループ
+        tweener = gameObject.GetComponent<RectTransform> ().DOLocalRotate (new Vector3 (x, y, z), rotateSecond, RotateMode.FastBeyond360)
+            .SetLoops (-1);
+        yield return null;
+    }
+}
---
+++ Library/Haccason/Assets/Script/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public int shotX;
+
+    public int shotY;
+
+    public int shotZ;
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            arow(new Vector3(shotX, shotY, shotZ));
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        Rotate.decide = true;
+    }
+    public void arow (Vector3 ar) {
+        GetComponent<Rigidbody> ().AddForce (ar);
+    }
+}
